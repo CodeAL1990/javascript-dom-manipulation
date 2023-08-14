d6cb49e43bcb2b30ad7b88c24dbc46f48e651bfc
FetusCoder <127114506+FetusCoder@users.noreply.github.com>
2023-08-14 14:45:53 +0700

    Boilerplate and stuff for DOM tutorial

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # javascript-dom-manipulation
+
+Part 1: Selecting items in a DOM
+Part 2: Styling elements using JS / DOM
+Part 3: Creating elements
---
+++ app.js
@@ -0,0 +1,52 @@
+/* Part 1 */
+
+// DOM Manipulation
+
+// GetElementById()
+
+// const title = document.getElementById("main-heading");
+
+// console.log(title);
+
+// GetElementByClassName()
+
+// const listItems = document.getElementsByClassName("list-items");
+
+// console.log(listItem);
+
+// getElementsByTagName()
+
+// const listItems = document.getElementsByTagName("li");
+
+// console.log(listItems);
+
+// querySelector()
+
+// const container = document.querySelector("div");
+
+// console.log(container);
+
+// querySelectorAll()
+
+// const containers = document.querySelectorAll("div");
+
+// console.log(containers);
+
+/* Part 2 */
+
+// const title = document.querySelector("#main-heading");
+
+// title.style.color = "red"; /* inline styling only works for 1 element and not multiple such as li*/
+
+// console.log(title);
+
+// const listItems = document.querySelectorAll(".list-items");
+
+// /* You will need a loop to select all items to do inline styling */
+// for (let i = 0; i < listItems.length; i++) {
+//   listItems[i].style.fontSize = "3rem";
+// }
+
+// console.log(listItems);
+
+/* Part 3 */
