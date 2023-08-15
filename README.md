87c1003d965c86f53c207c1f95a59d4f36c7b222
FetusCoder <127114506+FetusCoder@users.noreply.github.com>
2023-08-15 15:03:07 +0700

    Part 3 and Part 4

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 Part 1: Selecting items in a DOM
 Part 2: Styling elements using JS / DOM
-Part 3: Creating elements
+Part 3: Creating, modifying, and removing elements
+Part 4: Traversing the DOM
--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-/* Part 1 */
+/* Part 1: Selecting elements in a DOM */
 
 // DOM Manipulation
 
@@ -32,7 +32,7 @@
 
 // console.log(containers);
 
-/* Part 2 */
+/* Part 2: Styling elements */
 
 // const title = document.querySelector("#main-heading");
 
@@ -49,4 +49,83 @@
 
 // console.log(listItems);
 
-/* Part 3 */
+/* Part 3: Creating, modifying, and removing elements */
+
+// Create elements
+
+// const ul = document.querySelector("ul");
+// const li = document.createElement("li");
+
+// Adding Elements
+
+// ul.append(li);
+
+// Modifying the text
+
+// li.innerText = "X-men";
+
+// Modifying attributes
+
+// li.setAttribute("id", "main-heading");
+// li.removeAttribute("id");
+
+// const title = document.querySelector("#main-heading");
+
+// console.log(title.getAttribute("id"));
+
+// Modifying classes
+
+// li.classList.add("list-items");
+
+// console.log(li.classList.contains("list-items"));
+
+// Remove elements
+
+// li.remove();
+
+/* Part 4: Traversing the DOM */
+
+// Parent Node Traversal
+
+// let ul = document.querySelector("ul");
+
+/* Each parentNode/parentElement moves up a level */
+// console.log(ul.parentNode.parentNode);
+// console.log(ul.parentElement.parentElement);
+
+// const html = document.documentElement;
+
+/* parentNodes will retrieve any node while parentElement retrieves any element but returns null because document node is not an element node */
+// console.log(html.parentNode);
+// console.log(html.parentElement);
+
+// Child Node Traversal
+
+// let ul = document.querySelector("ul");
+
+/* Text nodes are child noes as well, and indentations count as text nodes */
+// console.log(ul.childNodes);
+// console.log(ul.firstChild);
+// console.log(ul.lastChild);
+
+// ul.childNodes[1].style.backgroundColor = "blue";
+
+/* These only select the list-items and not the text nodes */
+// console.log(ul.children);
+// console.log(ul.firstElementChild);
+// console.log(ul.lastElementChild);
+
+// Sibling Node Traversal
+
+// let ul = document.querySelector("ul");
+// const div = document.querySelector("div");
+
+// console.log(div.childNodes);
+
+/* Again, text nodes */
+// console.log(ul.previousSibling);
+// console.log(ul.nextSibling);
+
+/* If you only want the elements inside */
+// console.log(ul.previousElementSibling);
+// console.log(ul.nextElementSibling);
