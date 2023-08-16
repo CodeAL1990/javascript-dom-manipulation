7726ef8cf193ea94c29286c32310680df76b2ad8
FetusCoder <127114506+FetusCoder@users.noreply.github.com>
2023-08-16 15:25:28 +0700

    Part 5 and Part 6

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Part 1: Selecting items in a DOM
 Part 2: Styling elements using JS / DOM
 Part 3: Creating, modifying, and removing elements
 Part 4: Traversing the DOM
+Part 5: Event listeners
+Part 6: Reveal/hide using event listeners
--- app.js
+++ app.js
@@ -129,3 +129,43 @@
 /* If you only want the elements inside */
 // console.log(ul.previousElementSibling);
 // console.log(ul.nextElementSibling);
+
+/* Part 5: Event listeners */
+
+// example syntax --> element.addEventListener("click", function);
+
+// const buttonTwo = document.querySelector("#btn-2");
+
+// function alertBtn() {
+//   alert("I also love JavaScript");
+// }
+
+// buttonTwo.addEventListener("click", alertBtn);
+
+// // Mouseover
+
+// const newBackgroundColor = document.querySelector(".box-3");
+
+// function changeBgColor() {
+//   newBackgroundColor.style.backgroundColor = "blue";
+// }
+
+// newBackgroundColor.addEventListener("mouseover", changeBgColor);
+
+/* Part 6: Reveal/hide using event listeners */
+
+// Reveal Event
+
+const revealBtn = document.querySelector(".reveal-btn");
+
+const hiddenContent = document.querySelector(".hidden-content");
+
+function revealContent() {
+  if (hiddenContent.classList.contains("reveal-btn")) {
+    hiddenContent.classList.remove("reveal-btn");
+  } else {
+    hiddenContent.classList.add("reveal-btn");
+  }
+}
+
+revealBtn.addEventListener("click", revealContent);
